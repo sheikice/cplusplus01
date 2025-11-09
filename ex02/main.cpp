82f5a7790f3c53245c08c5cbe4e98b5506677d60
Jeremy Wuille <jwuille@f6r13s8.clusters.42paris.fr>
2025-11-09 17:17:29 +0100

    [clean] - ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,9 @@ int	main(void)
 	std::string		var = "HI THIS IS BRAIN";
 	std::string*	stringPTR = &var;
 	std::string&	stringREF = var;
+	std::string*	stringPTR2 = NULL;
 
+	stringPTR2 = &stringREF;
 	std::cout << "\033[1;32m"
 		<< "&var = " << &var << "\n"
 		<< "stringPTR = " << stringPTR << "\n"
@@ -19,4 +21,18 @@ int	main(void)
 		<< "stringPTR value pointed = " << *stringPTR << "\n"
 		<< "stringREF value pointed = " << stringREF << "\n"
 		<< "\033[0m" << std::endl;
+
+
+	// std::cout << "\033[1;32m"
+	// 	<< "&var = " << &var << "\n"
+	// 	<< "stringPTR2 = " << stringPTR2 << "\n"
+	// 	<< "stringREF = " << (void*)&stringREF << "\n"
+	// 	<< "\033[0m" << std::endl;
+	//
+	// std::cout << "\n"
+	// 	<< "\033[1;32m"
+	// 	<< "var = " << var << "\n"
+	// 	<< "stringPTR2 value pointed = " << *stringPTR2 << "\n"
+	// 	<< "stringREF value pointed = " << stringREF << "\n"
+	// 	<< "\033[0m" << std::endl;
 }
